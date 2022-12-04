a88e57199b33509ec006772faf9011b5e5483c7f
Matzu14 <maximilianoga38@gmail.com>
2022-12-04 14:01:39 -0600

    Updates

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-import firebaseConfig from "./firebaseConfig";
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
